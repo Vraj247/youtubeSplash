07e29b26b928252c3ad5a610dbb5551394b32333
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2024-04-20 21:26:26 +0530

    Initial Commit

---
+++ youtubeSplash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  youtubeSplash
+//
+//  Created by Vikas Yadav on 20/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ youtubeSplash/youtubeSplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtubeSplashApp.swift
+//  youtubeSplash
+//
+//  Created by Vikas Yadav on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct youtubeSplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
